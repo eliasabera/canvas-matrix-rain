03bc907df52b47c00b094a1ef3c6620da1942dc6
eliasabera <154503195+eliasabera@users.noreply.github.com>
2025-06-29 23:44:48 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Symbol{
+    constructor() {
+      
+  }
+}
+class Effect{
+    constructor() {
+       
+   }
+}
