0e8f0f746627bead3cacef644198b34ca4661d02
eliasabera <154503195+eliasabera@users.noreply.github.com>
2025-06-30 01:35:23 +0300

    update

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+let hue=0
+
 
 class Symbol {
   constructor(x, y, fontSize, canvasHeight) {
@@ -13,7 +15,7 @@ class Symbol {
   }
     draw(context) {
         this.text = this.character.charAt(Math.floor(Math.random() * this.character.length))
-        context.fillStyle = "#0aff0a ";
+        
         context.fillText(this.text, this.x*this.fontSize, this.y*this.fontSize);
         if (this.y * this.fontSize > this.canvasHeight && Math.random()>0.95) {
             this.y = 0;
@@ -39,10 +41,17 @@ class Effect{
             this.symbol[i]= new Symbol(i,0,this.fontsize,this.canvasHeight)
         }
     }
+    resize(width, height) {
+        this.canvasWidth = width;
+        this.canvasHeight = height;
+        this.column = this.canvasWidth / this.fontsize;
+        this.symbol = [];
+        this.#initialize()
+    }
 }
 const effect = new Effect(canvas.width,canvas.height)
 let lastTime = 0;
-const fps = 30;
+const fps = 15;
 const nextFrame = 1000 / fps;
 let timer = 0;
 
@@ -50,12 +59,23 @@ function animate(timeStamp) {
     const deltaTime = timeStamp - lastTime;
     lastTime = timeStamp;
     if (timer > nextFrame) {
-        
+        ctx.fillStyle = "rgba(0,0,0,0.05)";
+        ctx.textAlign="center"
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
+        ctx.fillStyle = "hsl(" + hue + ", 100%, 50% )";;
+        ctx.font = effect.fontsize + 'px monospace'
+        effect.symbol.forEach(symbo => symbo.draw(ctx))
+        timer = 0
+        hue++
     }
-    ctx.fillStyle = "rgba(0,0,0,0.05)";
-    ctx.fillRect(0,0,canvas.width,canvas.height)
-    ctx.font = effect.fontsize + 'px monospace'
-    effect.symbol.forEach(symbo => symbo.draw(ctx))
+    else {
+        timer += deltaTime;
+    }
     requestAnimationFrame(animate)
 }
-animate()
+animate(0)
+
+window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height=window.innerHeight
+})
