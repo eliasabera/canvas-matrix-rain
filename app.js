a897e174a85c9ae8fb92e62c8c91da73a746c88c
eliasabera <154503195+eliasabera@users.noreply.github.com>
2025-06-30 00:10:56 +0300

    update

--- app.js
+++ app.js
@@ -3,13 +3,37 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-class Symbol{
-    constructor() {
-      
+class Symbol {
+  constructor(x, y, fontSize, canvasHeight) {
+    this.character = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜｦﾝ!@#$%^&*()-_=+[]{}|;:',.<>/?`;
+    this.x = x;
+    this.y = y;
+    this.fontSize = fontSize;
+    this.canvasHeight = canvasHeight;
+  }
+    draw(context) {
+        this.text = this.character.charAt(Math.floor(Math.random() * this.character.length))
+        context.fillStyle = "0aff0a";
+        context.fillText(this.text, this.x*this.fontSize, this.y*this.fontSize);
+        if (this.y * this.fontSize > this.canvasHeight) {
+            this.y = 0;
+        }
+        else {
+            this.y += 1;
+        }
   }
 }
 class Effect{
-    constructor() {
-       
-   }
+    constructor(canvasWidth,canvasHeight) {
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+        this.fontsize = 25;
+        this.column = this.canvasWidth / this.fontsize;
+        this.symbol=[]
+    }
+    #initialize() {
+        for (let i = 0; i < this.column; i++){
+            this.Symbol[i]= new Symbol(i,0,this.fontsize,this.canvasHeight)
+        }
+    }
 }
